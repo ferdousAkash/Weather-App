b9cd3cd3b77f58ab8186001ffc9a8452f743bec1
ferdousAkash <ferdous.hasan01@northsouth.edu>
2022-12-27 17:28:47 +0600

    0

--- script.js
+++ script.js
@@ -5,8 +5,32 @@ const options = {
 		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
 	}
 };
-
-fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Dhaka', options)
+const getWeather = (city)=>{
+	cityName,innerHTML = city 
+	fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Dhaka', options)
 	.then(response => response.json())
-	.then(response => console.log(response))
+	.then((response) => {
+
+		console.log(response)
+		
+		cloud_pct.innerHTML = response.cloud_pct  
+		temp.innerHTML = response.temp 
+		feels_like.innerHTML = response.feels_like 
+		humidity.innerHTML = response.humidity 
+		min_temp.innerHTML = response.min_temp 
+		max_temp.innerHTML = response.max_temp 
+		wind_speed.innerHTML = response.wind_speed 
+		wind_degrees.innerHTML = response.wind_degrees 
+		sunrise.innerHTML = response.sunrise 
+		sunset.innerHTML = response.sunset 
+
+	})
 	.catch(err => console.error(err));
+}
+
+submit.addEventListener("click", (e)=>
+{
+	getWeather(city.value)
+})
+
+getWeather("")
